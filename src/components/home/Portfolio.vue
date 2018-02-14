0c1d3c06f3ea9f8e169ddf2142ef3106a3f9ff08
Hiras <mhmdhiras@gmail.com>
2018-02-14 17:10:21 +0530

    cleared the dummy data

--- src/components/home/Portfolio.vue
+++ src/components/home/Portfolio.vue
@@ -2,24 +2,15 @@
     <section class="section portfolio" id="portfolio">
         <h1>Portfolio</h1>
         <div class="tile is-ancestor">
-          <div class="portfolio-sites" v-for="(item, index) in testdata" :key='index'>
-            <h3>{{item.name}}</h3>
-            <img :src="item.image">
+          <div class="portfolio-sites">
+            <h3>To b filled</h3>
           </div>
         </div>
     </section>  
 </template>
 
 <script>
-export default {
-    data () {
-        return {
-            testdata: [
-                { name: 'Project1', image:'https://www.extremetech.com/wp-content/uploads/2018/02/Tesla-Model-3.jpg'},
-                { name: 'Project2', image:'https://cleantechnica.com/files/2017/01/Tesla-Model-3-red.png'}
-            ]
-        }
-    }
+export default {    
 }
 </script>
 
